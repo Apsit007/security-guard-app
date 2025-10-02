453467a043f9a8aafa6120f7e541753c071d1f61
Apsit007 <name.lod@gmail.com>
2025-10-02 14:25:42 +0700

    update : color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          "Sarabun",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+      },
+      colors: {
+        primary: "#124692",
+        "primary-light": "#A3CBF2",
+        "primary-dark": "#0D3063",
+        gold: "#F3B741",
+        "gold-light": "#ffcd6b",
+        "gold-dark": "#cc9427",
+      },
+    },
   },
   plugins: [],
 };
